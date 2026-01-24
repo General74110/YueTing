c7327bb9beb17012a3ded0e7b284c5821854b4a9
General74110 <6901777@gmail.com>
2026-01-24 22:22:40 +0800

    创建

--- config/novel.js
+++ config/novel.js
@@ -1,7 +1,18 @@
-{
-    "id": "yuetingba_xian_ni",
-    "title": "仙逆",
-    "status": "serial",
-    "lastSkip": -1,
-    "chapters": []
-}
+module.exports = {
+    site: 'yuetingba',
+
+    id: 'yuetingba_xian_ni',
+    title: '仙逆',
+    author: '耳根',
+    speaker: '边江工作室',
+
+    bookId: '3a1c0235-9335-5f9b-b236-e3b92dda9baa',
+
+    // 播放页模板
+    playUrl(skip) {
+        return `http://www.yuetingba.cn/book/detail/${this.bookId}/${skip}`;
+    },
+
+    startSkip: 0,
+    maxEmptyRetry: 3
+};
